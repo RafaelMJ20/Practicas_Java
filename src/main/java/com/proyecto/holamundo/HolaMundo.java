cf6e53865d6005c2769d6e789b3407df0d5e5ef7
Rafa <mirandarafa010@gmail.com>
2024-02-03 20:16:24 -0600

    Practica1

---
+++ src/main/java/com/proyecto/holamundo/HolaMundo.java
@@ -0,0 +1,9 @@
+package com.proyecto.holamundo;
+
+public class HolaMundo {
+    
+    public static void main(String args[]){
+        System.out.println("Hola Mundo.");
+        System.out.println("Otra linea de código.");
+    }
+}
